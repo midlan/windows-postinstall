c8fc309f0e551d019a17dbfaef9e19a131554053
Milan Davídek <me@milandavidek.cz>
2018-07-10 11:24:38 +0200

    Added docker install, hyper-v enable

---
+++ apps/docker.ps1
@@ -0,0 +1,6 @@
+﻿# install latest chrome
+cd $HOME\Downloads\
+
+(new-object System.Net.WebClient).DownloadFile('https://download.docker.com/win/stable/Docker%20for%20Windows%20Installer.exe', (Get-Item -Path '.\').FullName + '\DockerforWindowsInstaller.exe')
+cmd /C DockerforWindowsInstaller.exe
+rm DockerforWindowsInstaller.exe
---
+++ settings/enable_hyperv.ps1
@@ -0,0 +1,3 @@
+﻿# enable windows 10 Hyper-V feature (availible only in Pro version)
+
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All -NoRestartWrite-Output "Hyper-V enabled, to take it effect, restart system please."
