a48aa754562ef4162ad6be9e5c6dc65532764403
Milan Davídek <me@milandavidek.cz>
2018-08-05 22:37:20 +0200

    Franz app added

---
+++ apps/franz.ps1
@@ -0,0 +1,20 @@
+﻿# install latest franz
+cd $HOME\Downloads\
+
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+$wc = New-Object System.Net.WebClient
+$wc.Headers.Add("User-Agent: powershell")
+$json = $wc.DownloadString('https://api.github.com/repos/meetfranz/franz/releases/latest')
+$jsonObject = ConvertFrom-Json -InputObject $json
+$franzInstaller = ''
+
+Foreach ($asset in $jsonObject.assets) {
+    
+    if ($asset.name.endswith('.exe')) {
+        $franzInstaller = $asset.name
+        $wc.DownloadFile($asset.browser_download_url, (Get-Item -Path '.\').FullName + '\' + $franzInstaller)
+        cmd /C $franzInstaller /s
+        rm $franzInstaller
+        break
+    }
+}
