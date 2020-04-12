b1c85ae0ec0cc48c8e263bb2b7206482c31fd729
Milan Davídek <midlan@users.noreply.github.com>
2020-04-12 23:25:45 +0200

    comment fixed

--- apps/docker.ps1
+++ apps/docker.ps1
@@ -1,4 +1,4 @@
-﻿# install latest chrome
+﻿# install latest docker
 cd $HOME\Downloads\
 
 (new-object System.Net.WebClient).DownloadFile('https://download.docker.com/win/stable/Docker%20for%20Windows%20Installer.exe', (Get-Item -Path '.\').FullName + '\DockerforWindowsInstaller.exe')
